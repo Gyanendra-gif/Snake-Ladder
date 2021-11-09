89470e469aa6f6f29dbc0bf48bb6b18b0059665c
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-09 19:35:27 +0530

    [Gyanendra] Add: A Program to Repeat the Condition until player Win

--- Snack_Ladder_Problem/Snack_Ladder_Problem/SnakeLadderComputation.cs
+++ Snack_Ladder_Problem/Snack_Ladder_Problem/SnakeLadderComputation.cs
@@ -6,7 +6,7 @@ namespace Snack_Ladder_Problem
 {
     class SnakeLadderComputation
     {
-        public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1;
+        public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1, WIN = 100, START = 0;
         int playerPosition = 0;
         Random rand = new Random();
         public int diceRoll()
@@ -14,24 +14,28 @@ namespace Snack_Ladder_Problem
             int roll = rand.Next(1, 7);
             return roll;
         }
-        public void play() {
-            int Roll = this.diceRoll();
-            int Check = rand.Next(0, 3);
-            switch (Check)
+        public void play()
+        {
+            while (this.playerPosition < WIN)
             {
-                case NO_PLAY:
-                    break;
-                case LADDER:
-                    this.playerPosition += Roll;
-                    break;
-                case SNAKE:
-                    this.playerPosition -= Roll;
-                    if (this.playerPosition < 0)
-                    {
-                        this.playerPosition = 0;
-                    }
-                    break;
+                int Roll = this.diceRoll();
+                int Check = rand.Next(0, 3);
+                switch (Check)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.playerPosition += Roll;
+                        break;
+                    case SNAKE:
+                        this.playerPosition -= Roll;
+                        if (this.playerPosition < 0)
+                        {
+                            this.playerPosition = 0;
+                        }
+                        break;
+                }
             }
-        } 
+        }
     }
- }
+}
