ea0f28fde9d73dfe66dad418ceba3d0c41613f37
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-09 19:06:01 +0530

    [Gyanendra] Add: A Program Start where Player is at Zero Position

---
+++ Snack_Ladder_Problem/Snack_Ladder_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snack_Ladder_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeLadderComputation sl = new SnakeLadderComputation();
+            sl.diceRoll();
+        }
+    }
+    
+}
---
+++ Snack_Ladder_Problem/Snack_Ladder_Problem/SnakeLadderComputation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snack_Ladder_Problem
+{
+    class SnakeLadderComputation
+    {
+        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        int playerPosition = 0;
+        void diceRoll()
+        {
+            Console.WriteLine("InitialPosition of Player is: " + playerPosition);
+        }
+    }
+}
