a0fcd05a8d4f9e11a5570b10c26211e93234a181
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-10 12:01:04 +0530

    [Gyanendra] Add: A Program to Check Position of Player until it reached

--- Snack_Ladder_Problem/Snack_Ladder_Problem/SnakeLadderComputation.cs
+++ Snack_Ladder_Problem/Snack_Ladder_Problem/SnakeLadderComputation.cs
@@ -7,39 +7,46 @@ namespace Snack_Ladder_Problem
     class SnakeLadderComputation
     {
         public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1, WIN = 100, START = 0;
-        int playerPosition = 0;
+        int playerPosition = 0, count=0;
         Random rand = new Random();
-        public int diceRoll()
+        public int DiceRoll()
         {
-            int roll = rand.Next(1, 7);
-            return roll;
+            count++;
+            int rol = rand.Next(1, 7);
+            return rol;
         }
-        public void play()
+        public void Play()
         {
             while (this.playerPosition < WIN)
             {
-                int Roll = this.diceRoll();
+                int roll = this.DiceRoll();
                 int Check = rand.Next(0, 3);
                 switch (Check)
                 {
                     case NO_PLAY:
                         break;
                     case LADDER:
-                        this.playerPosition += Roll;
+                        this.playerPosition += roll;
                         if (this.playerPosition > WIN)
                         {
-                            this.playerPosition -= Roll;
+                            this.playerPosition -= roll;
+                            Console.WriteLine("Player is Close to Win");
                         }
                         break;
                     case SNAKE:
-                        this.playerPosition -= Roll;
+                        this.playerPosition -= roll;
                         if (this.playerPosition < START)
                         {
                             this.playerPosition = 0;
+                            Console.WriteLine("Sanke Bite");
                         }
                         break;
                 }
+                    count++;
+                Console.WriteLine("The Player Position is :" + this.playerPosition + " " + "with a roll of " +roll);
             }
+            Console.WriteLine("Number of times dice was Rolled to win the game is : " + count);
+            Console.WriteLine("The Player wins the game");
         }
     }
 }
